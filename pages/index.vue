c271c51b678b61bcae42c9d2b133ec3708d62de3
Danyvi <30378170+Danyvi@users.noreply.github.com>
2021-01-24 18:12:49 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h2>Welcome to the best corny dad jokes on the Web</h2>
+  </div>
+</template>
+
+<script>
+export default {
+	head() {
+		return {
+			title: 'Welcome to dad jokes',
+			meta: [
+				{
+					hid: 'description',
+					name: 'description',
+					content: 'best place for corny dad jokes'
+				}
+			]
+		}
+	}
+}
+</script>
